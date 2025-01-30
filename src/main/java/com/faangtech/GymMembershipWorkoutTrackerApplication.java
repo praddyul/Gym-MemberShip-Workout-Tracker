57a50275764f6d7f0a03296329aa911b3801678f
praddyul <nithigpradyul@gmail.com>
2025-01-30 17:40:06 +0530

    From intellij

---
+++ src/main/java/com/faangtech/GymMembershipWorkoutTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.faangtech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymMembershipWorkoutTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GymMembershipWorkoutTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/faangtech/GymMembershipWorkoutTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.faangtech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymMembershipWorkoutTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
